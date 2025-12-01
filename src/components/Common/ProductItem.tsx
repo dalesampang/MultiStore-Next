00bcdd948a250e61b1c78da8836d05d61a89f2d0
dalesampang <cdalesampang@gmail.com>
2025-12-02 01:55:30 +0800

    update
    update

--- src/components/Common/ProductItem.tsx
+++ src/components/Common/ProductItem.tsx
@@ -62,8 +62,7 @@ const ProductItem = ({ item }: { item: Product }) => {
 
       <h3 className="font-medium text-dark ease-out duration-200 hover:text-blue mb-1.5">
         <Link href={`/ai-glasses/${item.slug}`}>
-          {" "}
-          {item.name} {item.version}{" "}
+          {item.name}-{item.version}
         </Link>
       </h3>
 
