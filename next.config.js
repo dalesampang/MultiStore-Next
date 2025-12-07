4ed1c959b04966f5c99cc2300d26a1bc29186eee
dalesampang <cdalesampang@gmail.com>
2025-12-07 16:40:25 +0800

    Added Content Security Policy (CSP)
    Added Content Security Policy (CSP)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // ✅ allow Cloudinary
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' https://trusted.cdn.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
